39d156768f05ef4af36fbf621ed49a11c2dd485e
harshi <harshirai1408@gmail.com>
2025-07-14 13:12:48 +0530

    some changes

--- Backend/app.js
+++ Backend/app.js
@@ -19,7 +19,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["https://fit-and-flair-frontend.onrender.com"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
